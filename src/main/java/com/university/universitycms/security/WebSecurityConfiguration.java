205285ca438a6d169fc0261b0288147130d740ae
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2023-11-22 14:07:10 +0100

    fix simple login

--- src/main/java/com/university/universitycms/security/WebSecurityConfiguration.java
+++ src/main/java/com/university/universitycms/security/WebSecurityConfiguration.java
@@ -1,9 +1,8 @@
 package com.university.universitycms.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -14,21 +13,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfiguration {
-    private static final String FIND_USER = "SELECT user_name, user_surname, user_password FROM users WHERE user_name = ?;";
-
-    @Autowired
-    private DataSource dataSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/", "/students", "/teachers", "/courses", "/groups", "/departments").permitAll()
+                        .requestMatchers("/", "/webjars/**", "/static/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
@@ -39,15 +33,6 @@ public class WebSecurityConfiguration {
                 .logout(LogoutConfigurer::permitAll);
 
         return http.build();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder managerBuilder) throws Exception {
-        managerBuilder.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(new BCryptPasswordEncoder())
-                .usersByUsernameQuery(FIND_USER);
-
     }
 
     @Bean
