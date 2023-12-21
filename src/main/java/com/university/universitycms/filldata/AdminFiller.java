3dee05f8dc155082306503a3cba076c23befd639
Volodymyr Petrunin <petruninvolodymyr@gmail.com>
2023-12-21 13:51:39 +0100

    fix 4.0

--- src/main/java/com/university/universitycms/filldata/AdminFiller.java
+++ src/main/java/com/university/universitycms/filldata/AdminFiller.java
@@ -3,33 +3,36 @@ package com.university.universitycms.filldata;
 import com.university.universitycms.domain.Role;
 import com.university.universitycms.domain.Teacher;
 import com.university.universitycms.repository.TeacherRepository;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
 @Component
-@Profile("create-admin")
+@Profile("fill-dev-data")
 public class AdminFiller implements DataFiller {
 
     private final Teacher admin;
-
+    private final String password;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final TeacherRepository teacherRepository;
 
     public AdminFiller(TeacherRepository teacherRepository, @Value("${admin.name}") String name,
-                       @Value("${admin.surname}") String surname, @Value("${admin.cashed.password}") String password,
-                       @Value("${admin.email}") String email) {
+                       @Value("${admin.surname}") String surname, @Value("${admin.password}") String password,
+                       @Value("${admin.email}") String email, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.teacherRepository = teacherRepository;
+        this.password = password;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 
-        // message for mentor password 123 :) I forbid everyone else to use this password :)))
-        this.admin = new Teacher(null, Role.ADMIN, name, surname, password, Collections.emptySet(), email);
+        this.admin = new Teacher(null, Role.ADMIN, name, surname, null, Collections.emptySet(), email);
     }
 
     @Override
-    @PostConstruct
     public void fillData() {
+        admin.setPassword(bCryptPasswordEncoder.encode(password));
+
         teacherRepository.save(admin);
     }
 }
